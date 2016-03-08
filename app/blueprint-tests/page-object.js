71dfc977fa39b548fc9409b62c54e682bbd08fb0
Jerad Gallinger <jerad@jeradgallinger.ca>
2016-03-08 09:52:52 -0500

    Added failing blueprint test

---
+++ app/blueprint-tests/page-object.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-page-object/blueprint-tests/page-object';
---
+++ node-tests/blueprints/page-object-test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
+var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
+var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+
+describe('Acceptance: ember generate and destroy page-object', function() {
+  setupTestHooks(this);
+
+  it('generates a page-object blueprint in an ember app', function() {
+    // pass any additional command line options in the arguments array
+    return generateAndDestroy(['page-object', 'foo'], {
+      // define files to assert, and their contents
+      target: 'app',
+      files: [{
+        file: 'tests/pages/foo.js',
+        contents: [
+          "import PageObject from 'frontend/tests/page-object';",
+          'let {',
+          '  visitable',
+          '} = PageObject;',
+          'export default PageObject.create({',
+          "  visit: visitable('/')'",
+          '});'
+        ]
+      }]
+    });
+  });
+
+  it('generates a page-object blueprint in an ember addon', function() {
+    // pass any additional command line options in the arguments array
+    return generateAndDestroy(['page-object', 'bar'], {
+      // define files to assert, and their contents
+      target: 'addon',
+      files: [{
+        file: 'tests/pages/foo.js',
+        contents: [
+          "import PageObject from 'dummy/tests/page-object';",
+          'let {',
+          '  visitable',
+          '} = PageObject;',
+          'export default PageObject.create({',
+          "  visit: visitable('/')'",
+          '});'
+        ]
+      }]
+    });
+  });
+});
